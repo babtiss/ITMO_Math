cc521feae09326a6d1a2bd5590bf5e2a50b427ed
Babtis <48028537+babtiss@users.noreply.github.com>
2021-04-10 21:53:44 +0300

    Update task_1f.py

--- 1 course/Homework_1_GraphTheory/task_1f.py
+++ 1 course/Homework_1_GraphTheory/task_1f.py
@@ -10,7 +10,7 @@ fin_edges = open("distances.txt")
 country = []  # список стран
 edges = []  # список рёбер
 G = nx.Graph()  # граф
-matrix = [[] for i in range(V)]  # матрица смежности
+matrix = [[] for i in range(V)]  
 for i in range(V):
     pars_one = fin_country.readline().split('.')
     if i != V - 1:
