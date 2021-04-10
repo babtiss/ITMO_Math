4627617e74a7e81efc674ee9146fcdcb7cb931f9
Babtis <48028537+babtiss@users.noreply.github.com>
2021-04-10 21:54:35 +0300

    Update task_1g.py

--- 1 course/Homework_1_GraphTheory/task_1g.py
+++ 1 course/Homework_1_GraphTheory/task_1g.py
@@ -11,7 +11,7 @@ country = []  # список стран
 edges = []  # список рёбер
 edges_name = []
 G = nx.Graph()  # граф
-matrix = [[] for i in range(V)]  # матрица смежности
+matrix = [[] for i in range(V)] 
 for i in range(V):
     pars_one = fin_country.readline().split('.')
     if i != V - 1:
