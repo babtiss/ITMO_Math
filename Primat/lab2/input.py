c0dae1f5e9013aebcfb1f3d5b76512e668a6175d
Babtis <48028537+babtiss@users.noreply.github.com>
2022-04-18 16:20:11 +0300

    Update input.py

--- Primat/lab2/input.py
+++ Primat/lab2/input.py
@@ -3,11 +3,6 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def f(x):
-    return math.e ** (math.sin(x) * math.log(x, math.e))
-
-
-# Наша двумерная функция
 
 
 # Производные по аргументам
