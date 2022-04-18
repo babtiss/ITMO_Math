de205a7a915d37df501a24578942959ebb338a37
Babtis <48028537+babtiss@users.noreply.github.com>
2022-04-18 16:50:09 +0300

    Update task5.py

--- Primat/lab2/task5.py
+++ Primat/lab2/task5.py
@@ -1,12 +1,9 @@
-import numpy as np
-from matplotlib import pyplot as plt
-
 from input import *
 
 
 def _golden_selection(a, b, xn, yn, px, py, _f):
-    k1 = (3 - math.sqrt(5)) / 2
-    k2 = (math.sqrt(5) - 1) / 2
+    k1 = K1
+    k2 = K2
 
     lambda1 = a + k1 * (b - a)
     lambda2 = a + k2 * (b - a)
@@ -21,19 +18,13 @@ def _golden_selection(a, b, xn, yn, px, py, _f):
         if f1 < f2:
             b = lambda2
             lambda2 = lambda1
-            f2 = f1
             lambda1 = a + k1 * (b - a)
             xn = xn + lambda1 * px
             yn = yn + lambda1 * py
-            f1 = _f(xn, yn)
         else:
             a = lambda1
             lambda1 = lambda2
-            xn_ = xn + lambda2 * px
-            yn_ = yn + lambda2 * py
-            f1 = _f(xn_, yn_)
             lambda2 = a + k2 * (b - a)
-            f2 = _f(xn_, yn_)
     return (a + b) / 2
 
 
@@ -51,22 +42,21 @@ def fletcher_reeves_method(func):
     yn = B
     fn = func(xn, yn)
     count = 0
-
-    # заводим словарь, где будем хранить все найденные значения функции
-
-    X = []
-    Y = []
-    F = []
     grad_x = -derivative_x(xn, yn)
     grad_y = -derivative_y(xn, yn)
     grad_square = grad_x ** 2 + grad_y ** 2
-    X.append(xn)
-    Y.append(yn)
-    F.append(fn)
+
+    # заводим словарь, где будем хранить все найденные значения функции
+    X = []
+    Y = []
+    F = []
+    draw_save(xn, yn, fn, X, Y, F)
+
     # по формуле градиентного спуска получаем все значения x y
-    count = 0
     while grad_square > epsilon:
         count += 1
+
+        # методом золотого сечения находим оптимальный lambda
         lambda_ = _golden_selection(0, 0.5, xn, yn, grad_x, grad_y, func)
         xn = xn + lambda_ * grad_x
         yn = yn + lambda_ * grad_y
@@ -75,21 +65,19 @@ def fletcher_reeves_method(func):
         grad_new_y = -derivative_y(xn, yn)
         grad_new_square = grad_new_x ** 2 + grad_new_y ** 2
 
-        if count == 10:
-
-            count = 0
+        # сбрасываем betta каждые 10 итераций
+        if count % 10 == 0:
             beta = 0
         else:
             beta = grad_new_square / grad_square
+
         grad_x = grad_new_x + beta * grad_x
         grad_y = grad_new_y + beta * grad_y
         grad_square = grad_x ** 2 + grad_y ** 2
 
         fn = func(xn, yn)
 
-        X.append(xn)
-        Y.append(yn)
-        F.append(fn)
+        draw_save(xn, yn, fn, X, Y, F)
         print(fn, xn, yn)
 
     print(count)
@@ -97,4 +85,3 @@ def fletcher_reeves_method(func):
 
 
 draw_function_plot(fletcher_reeves_method(func=f_dimensional))
-
